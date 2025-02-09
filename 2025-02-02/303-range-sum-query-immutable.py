421e6439c04cae107aaefe523e13307844d47e3e
Juan Lopes <juan.lopes@zwift.com>
2025-02-09 14:10:30 -0300

    Rename files

--- 2025-02-02/303-range-sum-query-immutable.py
+++
@@ -1,9 +0,0 @@
-#https://leetcode.com/problems/range-sum-query-immutable/
-class NumArray:
-    def __init__(self, nums):
-        self.nums = [0] + nums
-        for i in range(1, len(self.nums)):
-            self.nums[i] += self.nums[i - 1]
-        
-    def sumRange(self, left: int, right: int) -> int:
-        return self.nums[right+1] - self.nums[left]
