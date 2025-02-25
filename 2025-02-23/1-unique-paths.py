65b4715de0a8de97bc8d8736097b20a8c54cc441
Juan Lopes <juan.lopes@zwift.com>
2025-02-25 10:26:21 -0300

    Fixes

--- 2025-02-23/1-unique-paths.py
+++ 2025-02-23/1-unique-paths.py
@@ -1,7 +1,6 @@
 class Solution:
     @functools.cache
     def uniquePaths(self, m, n):
-        if m <= 0 or n <= 0: return 0
-        if m == 1 and n == 1: return 1
+        if m == 1 or n == 1: return 1
         return self.uniquePaths(m-1, n) + self.uniquePaths(m, n-1)
         
--- 2025-02-23/4-maximal-square.py
+++ 2025-02-23/4-maximal-square.py
@@ -2,7 +2,6 @@ class Solution:
     def maximalSquare(self, matrix):
         @functools.cache
         def solve(i, j):
-            if not 0 <= i < len(matrix) or not 0 <= j < len(matrix[i]): return 0
-            if matrix[i][j] == "0": return 0
+            if i < 0 or j < 0 or matrix[i][j] == "0": return 0
             return 1+min(solve(i-1, j-1), solve(i-1, j), solve(i, j-1))
         return max(solve(i, j)**2 for i in range(len(matrix)) for j in range(len(matrix[i])))
